ec84a4d4308d780db7f8fda2ad46800d5dc7e59d
Achyuth Mohan <achyuthmohan1@gmail.com>
2022-08-06 00:08:33 +0530

    custom user created

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path,include
 from rest_framework.routers import DefaultRouter
-from .views import JobViewSet,TrendingViewSet
+from .views import JobViewSet,TrendingViewSet,RegisterView
 router=DefaultRouter()
 router.register('jobs',JobViewSet, basename='jobs')
 router.register('trending',TrendingViewSet,basename='trending')
+# router.register('register',RegisterView,basename='trending')
 urlpatterns = [
     path('',include(router.urls)),
+    path('register/',RegisterView.as_view(),name="register")
 ]
--- api/views.py
+++ api/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
 from .serializers import JobSerializer,TrendingSerializer
-from .models import Job,Trending
-from rest_framework import generics,mixins,viewsets
-
+from .models import Job,Trending,User
+from rest_framework import generics,mixins,viewsets,status
+from .serializers import RegisterSerializer
+from rest_framework.response import Response
 # Create your views here.
+
+class RegisterView(generics.GenericAPIView):
+    serializer_class=RegisterSerializer
+
+    def post(self,request):
+        user=request.data
+        serializer=self.serializer_class(data=user)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        user_data=serializer.data
+        return Response(user_data,status=status.HTTP_201_CREATED)
+
 class JobViewSet(viewsets.GenericViewSet,mixins.ListModelMixin,mixins.RetrieveModelMixin):
     serializer_class=JobSerializer
     queryset=Job.objects.all()
