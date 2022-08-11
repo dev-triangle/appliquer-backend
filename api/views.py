538a3b4134c4cac7e9a5fdfbe841a8fa5210798f
Achyuth Mohan <achyuthmohan1@gmail.com>
2022-08-11 23:53:32 +0530

    permission for current user added

--- api/views.py
+++ api/views.py
@@ -15,6 +15,7 @@ class RegisterView(viewsets.GenericViewSet,mixins.CreateModelMixin):
 
 
 class LoggedInUserView(APIView):
+    permission_classes=[IsAuthenticated]
     def get(self, request):
         serializer = UserSerializer(request.user)
         return Response(serializer.data)
