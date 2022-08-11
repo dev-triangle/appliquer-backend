566cf5ae7ad99e79e4ca143ffda62e530517f0ac
Achyuth Mohan <achyuthmohan1@gmail.com>
2022-08-11 15:48:05 +0530

    application api

--- api/urls.py
+++ api/urls.py
@@ -2,12 +2,13 @@ from django.contrib import admin
 from django.urls import path,include
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
-from .views import JobViewSet,TrendingViewSet,RegisterView,UserDetailViewset,LogoutAuth
+from .views import JobViewSet,TrendingViewSet,RegisterView,UserDetailViewset,LogoutAuth,ApplicationViewset
 router=DefaultRouter()
 router.register('jobs',JobViewSet, basename='jobs')
 router.register('trending',TrendingViewSet,basename='trending')
 router.register('register',RegisterView,basename='register')
 router.register('user-detail',UserDetailViewset,basename='user-detail')
+router.register('application',ApplicationViewset,basename='application')
 urlpatterns = [
     path('',include(router.urls)),
     path('api/token/',TokenObtainPairView.as_view(),name="token_obtain"),
